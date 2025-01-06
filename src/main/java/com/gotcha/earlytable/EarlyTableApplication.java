9c464856ba6cdb1c84c8a0ed5416762a897931fc
taketheking <so4524@gmail.com>
2025-01-06 09:36:50 +0900

    🎉 Begin : Create Project

---
+++ src/main/java/com/gotcha/earlytable/EarlyTableApplication.java
@@ -0,0 +1,13 @@
+package com.gotcha.earlytable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarlyTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarlyTableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gotcha/earlytable/EarlyTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotcha.earlytable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarlyTableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
