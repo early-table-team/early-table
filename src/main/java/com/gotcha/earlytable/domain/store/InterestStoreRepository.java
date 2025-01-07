257856d9648bd8d8e02638d56203b650ebb33b59
MinSeok3796 <ms3796@naver.com>
2025-01-07 23:11:39 +0900

    fix : Change Repository type

--- src/main/java/com/gotcha/earlytable/domain/store/InterestStoreRepository.java
+++ src/main/java/com/gotcha/earlytable/domain/store/InterestStoreRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InterestStoreRepository extends JpaRepository<InterestStore, Integer> {
+public interface InterestStoreRepository extends JpaRepository<InterestStore, Long> {
 }
