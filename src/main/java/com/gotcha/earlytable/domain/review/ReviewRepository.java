e361276f53f66977ba8d7b3faf3b90ead0344212
yujinly <76680073+yujlee31@users.noreply.github.com>
2025-01-08 18:51:30 +0900

    Feat : Create Review API
    * Feat : Create Review API
    
    * Feat : Modify deleteReview from HardDelete to SoftDelete using Enum on ReviewStatus

--- src/main/java/com/gotcha/earlytable/domain/review/ReviewRepository.java
+++ src/main/java/com/gotcha/earlytable/domain/review/ReviewRepository.java
@@ -1,12 +1,16 @@
 package com.gotcha.earlytable.domain.review;
 
+import com.gotcha.earlytable.domain.review.dto.ReviewTotalResponseDto;
 import com.gotcha.earlytable.domain.review.entity.Review;
 import com.gotcha.earlytable.domain.store.entity.Store;
 import com.gotcha.earlytable.global.error.ErrorCode;
 import com.gotcha.earlytable.global.error.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -21,4 +25,19 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Long countReviewsByStore(Store store);
 
+    List<Review> findAllByStoreStoreId(Long storeId);
+
+    List<Review> findAllByUserId(Long userId);
+
+    // 선택 가게 리뷰들의 리뷰 별점 통계
+    @Query("select sum(case when r.rating = 1 then 1 else 0 end) as ratingStat1," +
+            "      sum(case when r.rating = 2 then 1 else 0 end) as ratingStat2," +
+            "      sum(case when r.rating = 3 then 1 else 0 end) as ratingStat3," +
+            "      sum(case when r.rating = 4 then 1 else 0 end) as ratingStat4," +
+            "      sum(case when r.rating = 5 then 1 else 0 end) as ratingStat5," +
+            "      count(r.rating) as countTotal," +
+            "      round(avg(r.rating),1) as ratingAverage" +
+            "  from Review r" +
+            " where r.store.storeId = :storeId ")
+    ReviewTotalResponseDto findStatisticsByStoreId(@Param("storeId") Long storeId);
 }
---
+++ src/main/java/com/gotcha/earlytable/domain/review/enums/ReviewStatus.java
@@ -0,0 +1,5 @@
+package com.gotcha.earlytable.domain.review.enums;
+
+public enum ReviewStatus {
+    NORMAL, DELETE
+}
