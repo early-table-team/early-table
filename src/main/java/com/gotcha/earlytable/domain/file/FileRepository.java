d12217a20e1b15609ef308e6b1e5d4505db25884
taketheking <so4524@gmail.com>
2025-01-08 15:20:49 +0900

    Feat : Create store update API

--- src/main/java/com/gotcha/earlytable/domain/file/FileRepository.java
+++ src/main/java/com/gotcha/earlytable/domain/file/FileRepository.java
@@ -1,8 +1,8 @@
 package com.gotcha.earlytable.domain.file;
 
 import com.gotcha.earlytable.domain.file.entity.File;
-import com.gotcha.earlytable.domain.file.entity.ImageFile;
-import com.gotcha.earlytable.domain.store.entity.Store;
+import com.gotcha.earlytable.global.error.ErrorCode;
+import com.gotcha.earlytable.global.error.exception.NotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +12,9 @@ import java.util.List;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
+
+    default File findByIdOrElseThrow(Long id){
+        return findById(id).orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND));
+    }
 
 }
