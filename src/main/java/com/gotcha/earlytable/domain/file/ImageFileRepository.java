71f83397647bf04b438d786aabfda1af4a90fdbd
MinSeok3796 <127212760+MinSeok3796@users.noreply.github.com>
2025-01-08 13:30:23 +0900

    Feat : Create getInterestStores API
    * Feat : Create getInterestStores API
    
    * fix : Repository jpa method
    
    * fix : repository content modify
    
    * fix : modify response status

--- src/main/java/com/gotcha/earlytable/domain/file/ImageFileRepository.java
+++ src/main/java/com/gotcha/earlytable/domain/file/ImageFileRepository.java
@@ -1,16 +1,15 @@
 package com.gotcha.earlytable.domain.file;
 
-import com.gotcha.earlytable.domain.file.entity.File;
 import com.gotcha.earlytable.domain.file.entity.ImageFile;
-import com.gotcha.earlytable.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface FileRepository extends JpaRepository<File, Long> {
+public interface ImageFileRepository extends JpaRepository<ImageFile, Long> {
 
+
+    @Query("SELECT i FROM ImageFile i JOIN i.file f JOIN Store s ON s.file = f WHERE s.storeId = :storeId AND i.fileStatus = :fileStatus")
+    ImageFile findByFileStoreStoreIdAndFileStatus(Long storeId, FileStatus fileStatus);
 }
