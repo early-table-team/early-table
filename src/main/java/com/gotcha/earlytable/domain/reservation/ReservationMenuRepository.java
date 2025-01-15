b4d29b7ae51972e92921ef52ea857e9264589a9f
MinSeok3796 <ms3796@naver.com>
2025-01-15 09:35:54 +0900

    Fix : Modify code in reservationAPI

--- src/main/java/com/gotcha/earlytable/domain/reservation/ReservationMenuRepository.java
+++ src/main/java/com/gotcha/earlytable/domain/reservation/ReservationMenuRepository.java
@@ -1,9 +1,12 @@
 package com.gotcha.earlytable.domain.reservation;
 
+import com.gotcha.earlytable.domain.reservation.entity.Reservation;
 import com.gotcha.earlytable.domain.reservation.entity.ReservationMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationMenuRepository extends JpaRepository<ReservationMenu, Long> {
+    void deleteAllByReservation(Reservation reservation);
+
 }
