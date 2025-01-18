b0f67e348875c59562e5743f0ac2ae343dfa40cb
taketheking <so4524@gmail.com>
2025-01-18 23:39:08 +0900

    Feat : Check the number of users currently store viewing

--- src/main/java/com/gotcha/earlytable/domain/store/StoreViewController.java
+++ src/main/java/com/gotcha/earlytable/domain/store/StoreViewController.java
@@ -1,19 +1,8 @@
 package com.gotcha.earlytable.domain.store;
 
-import jakarta.servlet.http.HttpServletResponse;
-import org.redisson.api.RAtomicLong;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import reactor.core.publisher.Flux;
-
-import java.io.IOException;
-import java.time.Duration;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executors;
-
 
 @RestController
 @RequestMapping("/store")
@@ -27,44 +16,44 @@ public class StoreViewController {
 
     /**
      * 해당 가게를 조회하면 이벤트 처리로 호출되는 API
-     *
      * 현재 가게를 보고있는 사람 수 ++
      *
      * @param storeId
-     * @return
+     * @return 가게 접속자 수 조회 시작 메세지
      */
     @PostMapping("/{storeId}/view/start")
     public ResponseEntity<String> startViewing(@PathVariable String storeId) {
+        // 조회 시작 이벤트 발행
+        storeViewService.publishStartViewingEvent(storeId);
 
-        long count = storeViewService.startViewing(storeId);
-
-        return ResponseEntity.ok("View started for store: " + storeId + ", Current View Count: " + count);
+        return ResponseEntity.ok("View started for store: " + storeId);
     }
 
     /**
      * 해당 가게 조회에서 나가면 이벤트 처리로 호출되는 API
-     *
      * 현재 가게를 보고있는 사람 수 --
      *
      * @param storeId
-     * @return
+     * @return 가게 접속자 수 조회 멈춤 메세지
      */
     @PostMapping("/{storeId}/view/stop")
     public ResponseEntity<String> stopViewing(@PathVariable String storeId) {
+        // 조회 종료 이벤트 발행
+        storeViewService.publishStopViewingEvent(storeId);
 
-        long count = storeViewService.stopViewing(storeId);
-
-        return ResponseEntity.ok("View stopped for store: " + storeId + ", Current View Count: " + count);
+        return ResponseEntity.ok("View stopped for store: " + storeId);
     }
 
-
-    // 실시간 접속자 수 스트리밍
-    @GetMapping(value = "/{storeId}/view/stream")
+    /**
+     * 실시간 접속자 수 스트리밍
+     *
+     * @param storeId
+     * @return 접속자 수
+     */
+    @GetMapping(value = "/{storeId}/view/stream", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Long> streamViewCount(@PathVariable String storeId) {
-
+        // 현재 조회 수를 Redis에서 가져옴
         Long count = storeViewService.getCurrentViewCount(storeId);
-
         return ResponseEntity.ok(count);
     }
-
 }
