2adc47004bdf8e0435478060c07aa1b05b8f6745
taketheking <so4524@gmail.com>
2025-01-27 11:34:58 +0900

    fix : change response data for front

--- src/main/java/com/gotcha/earlytable/global/enums/ReservationStatus.java
+++ src/main/java/com/gotcha/earlytable/global/enums/ReservationStatus.java
@@ -1,12 +1,15 @@
 package com.gotcha.earlytable.global.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum ReservationStatus {
 
     PENDING("예정"),
     COMPLETED("완료"),
     CANCELED("취소");
 
-    private String value;
+    private final String value;
 
     ReservationStatus(String value) {
         this.value = value;
