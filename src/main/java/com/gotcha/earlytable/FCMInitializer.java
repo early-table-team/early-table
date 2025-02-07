767fd88110228b466f27057855b4a318d3059966
taketheking <so4524@gmail.com>
2025-02-08 01:31:05 +0900

    Fix : change firebaseConfigPath

--- src/main/java/com/gotcha/earlytable/FCMInitializer.java
+++ src/main/java/com/gotcha/earlytable/FCMInitializer.java
@@ -3,35 +3,38 @@ package com.gotcha.earlytable;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.gotcha.earlytable.global.error.ErrorCode;
+import com.gotcha.earlytable.global.error.exception.CustomException;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.checkerframework.checker.units.qual.C;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 @Slf4j
 public class FCMInitializer {
 
-    @Value("${FIREBASE_CONFIG_PATH}")
-    private String firebaseConfigPath;
-
     @PostConstruct //빈 객체가 생성되고 의존성 주입이 완료된 후에 초기화가 실행될 수 있도록 @PostConstructor 설정
     public void initialize() {
-        try {
-            GoogleCredentials googleCredentials = GoogleCredentials
-                    .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream());
-            FirebaseOptions options = new FirebaseOptions.Builder()
+
+        String firebaseConfigPath = System.getenv("FIREBASE_CONFIG_PATH");
+        if (firebaseConfigPath == null) {
+            throw new CustomException(ErrorCode.BAD_REQUEST);
+        }
+        try (InputStream serviceAccount = new FileInputStream(firebaseConfigPath)) {
+            GoogleCredentials googleCredentials = GoogleCredentials.fromStream(serviceAccount);
+            FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(googleCredentials)
                     .build();
-            if(FirebaseApp.getApps().isEmpty()) {
+            if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
                 log.info("Firebase application has been initialized");
             }
-        } catch (IOException e){
-            log.error(e.getMessage());
+        } catch (IOException e) {
+            log.error("Error initializing Firebase", e);
         }
     }
 }
