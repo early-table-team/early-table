9a5653136971936208bbcf1048f774b9d9c27101
taketheking <so4524@gmail.com>
2025-02-08 18:18:44 +0900

    Feat : add pagination for storeSearch

--- src/main/java/com/gotcha/earlytable/domain/store/StoreRepositoryQuery.java
+++ src/main/java/com/gotcha/earlytable/domain/store/StoreRepositoryQuery.java
@@ -1,12 +1,12 @@
 package com.gotcha.earlytable.domain.store;
 
-import com.gotcha.earlytable.domain.store.dto.StoreListResponseDto;
 import com.gotcha.earlytable.domain.store.dto.StoreSearchRequestDto;
 import com.gotcha.earlytable.domain.store.dto.StoreSearchResponseDto;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface StoreRepositoryQuery {
 
-    List<StoreSearchResponseDto> searchStoreQuery(StoreSearchRequestDto requestDto);
+    List<StoreSearchResponseDto> searchStoreQuery(StoreSearchRequestDto requestDto, Pageable pageable);
 }
