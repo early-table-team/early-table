3ab421639c71cf1322bd09675bc0df980d3df272
taketheking <so4524@gmail.com>
2025-02-08 10:40:22 +0900

    Fix : change firebaseConfigPath

--- src/main/java/com/gotcha/earlytable/FCMInitializer.java
+++ src/main/java/com/gotcha/earlytable/FCMInitializer.java
@@ -29,6 +29,8 @@ public class FCMInitializer {
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(googleCredentials)
                     .build();
+
+
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
                 log.info("Firebase application has been initialized");
