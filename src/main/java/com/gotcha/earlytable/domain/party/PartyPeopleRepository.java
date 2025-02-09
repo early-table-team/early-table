1cd0123a2f7f6ae802e7baf13bf36abe49f42dc0
taketheking <so4524@gmail.com>
2025-02-10 01:18:05 +0900

    Fix : Reservation And Waiting Pagination Logic

--- src/main/java/com/gotcha/earlytable/domain/party/PartyPeopleRepository.java
+++ src/main/java/com/gotcha/earlytable/domain/party/PartyPeopleRepository.java
@@ -3,8 +3,6 @@ package com.gotcha.earlytable.domain.party;
 import com.gotcha.earlytable.domain.party.entity.Party;
 import com.gotcha.earlytable.domain.party.entity.PartyPeople;
 import com.gotcha.earlytable.domain.user.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +11,6 @@ import java.util.List;
 @Repository
 public interface PartyPeopleRepository extends JpaRepository<PartyPeople, Long> {
 
-    Page<PartyPeople> findByUser(User user, Pageable pageable);
 
     void deleteByUser(User user);
 
